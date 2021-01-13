f0070b4833a4d195f094706ec101c08f9d91c25c
Stephen Ndele <stephenndele09346@gmail.com>
2021-01-13 14:24:57 +0300

    Added function that appends user input

--- js/script.js
+++ js/script.js
@@ -9,4 +9,16 @@ $(document).ready(function() {
         $("#output").text(result);
  
 });
+$("form#feedback-form").on('submit', function(event) {
+    event.preventDefault();
+    let user_name = $("input#user_name").val();
+    let feedback = $("textarea#feedback").val();
+
+    $("#review").append("<div class='carousel-item'>"+"<p>"  + feedback + "</p> "+"<p>" + user_name + "</p>"+"</div>" );
+
+
+});
+// $("form#feedback-form").on('submit', function(event){
+// $("#review").append("<div class='carousel-item'</div>"  + "<div class='controls'" + feedback);
+// })
 });
